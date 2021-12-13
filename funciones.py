bc002d6fa3d4ad8f80cd32645ce90f5a869ed472
Tomás Morán Benitez <tmoranb@gmail.com>
2021-12-13 17:56:05 -0300

    funciones

--- funciones.py
+++ funciones.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 def plot_hist(dataframe, variable):
     
@@ -24,3 +29,26 @@ def graf_barra(dataframe, variable):
              5: 'white'}
     sns.catplot(x = variable, data = dataframe, kind = 'count', order = dataframe[variable].value_counts().index)
     plt.show()  
+
+def report_scores(y_testeo, y_prediccion):
+    mse = mean_squared_error(y_testeo, y_prediccion).round(1)
+    r2 = r2_score(y_testeo, y_prediccion).round(2)
+    
+    print('Error Cuadrático Promedio (MSE):', mse)
+    print('R2:', r2) 
+
+def fetch_features(dataframe, variable_objetivo):
+    columns = dataframe
+    nombre_var =[]
+    pearson_corr =[]
+    abs_pearson_corr = []
+    for col in columns:
+        if col != variable_objetivo:
+            nombre_var.append(col)
+            pearson_corr.append(dataframe[col].corr(dataframe[variable_objetivo]))
+            abs_pearson_corr.append(abs(dataframe[col].corr(dataframe[variable_objetivo])))
+            
+    features = pd.DataFrame({'variables': nombre_var, 'correlaciones': pearson_corr, 'abs_corr': abs_pearson_corr})
+    features2 = features.set_index('variables')
+    features3= features2.sort_values(by = ['abs_corr'], ascending = False)
+    return features3
